fc4c42391004501173174c7fdf3facf879f00eee
Alex <jahn.alexander@gmail.com>
2020-04-28 18:57:55 +0200

    Create DesmartifyGoogleSearch.js

---
+++ DesmartifyGoogleSearch.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name     Desmartify google search buttons
+// @version  1
+// @grant    none
+// @include  https://www.google.com/search*
+// ==/UserScript==
+
+let seq = ["Images", "Videos", "Maps", "News", "Shopping"];
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+function SortButtons() {
+    let tosort = document.querySelector("#hdtb-msb-vis");
+
+    let desiredSeq = [...tosort.children].sort((a, b) => seq.indexOf(a.innerText) > seq.indexOf(b.innerText) ? 1 : -1);
+    if (!arraysEqual(tosort.children, desiredSeq)) {
+        console.log("DesmartifyGoogleSearch: Sorting the buttons");
+        [...tosort.children].sort((a, b) => seq.indexOf(a.innerText) > seq.indexOf(b.innerText) ? 1 : -1).map(node => tosort.appendChild(node));
+    }
+}
+setInterval(SortButtons, 50);
